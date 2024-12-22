1a9a82b9813c91d5baf1648d593dadc2374ebc91
dhiahaddad <dhiahad@gmail.com>
2024-12-22 20:32:32 +0100

    Better design

--- gui.py
+++ gui.py
@@ -2,64 +2,77 @@ from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
     QWidget,
+    QHBoxLayout,
     QVBoxLayout,
 )
 
-from gui_widgets import InOutField, PushButton
+from gui_widgets import GroupBox, PushButton, InputField, OutputField
 
 
 class MainWindow(QWidget):
     resetButtonClicked = pyqtSignal()
-    submitButtonClicked = pyqtSignal(str,int)
+    submitButtonClicked = pyqtSignal(str, int)
     neverReaskButtonClicked = pyqtSignal()
 
     def __init__(self) -> None:
         super().__init__()
 
         self.setWindowTitle("Vocabulary Learner")
-        self.setGeometry(100, 100, 600, 600)
+        self.setGeometry(100, 100, 800, 600)
 
         self.__layout = QVBoxLayout()
 
-        element_height = int(self.height() * 0.1)
-        font_size = int(element_height * 0.2)
+        test_widget = GroupBox(self)
+        response_widget = GroupBox(self)
+
+        topHBoxLayout = QHBoxLayout()
+        topHBoxLayout.addWidget(test_widget, 1)
+        topHBoxLayout.addWidget(response_widget, 1)
+        self.__layout.addLayout(topHBoxLayout)
+
+        element_height = int(self.height() * 0.2)
+        font_size = int(element_height * 0.1)
         font = QFont("Sanserif", font_size)
 
-        self.__original_word = InOutField(
-            "Original word", font, element_height, readonly=True, parent=self
+        self.__original_word = OutputField(
+            "Original word", font, element_height, parent=self
         )
-        self.__layout.addWidget(self.__original_word)
+        test_widget.addWidget(self.__original_word)
 
-        self.__input_field = InOutField(
+        self.__input_field = InputField(
             "Enter your text here", font, element_height, parent=self
         )
-        self.__input_field.returnPressed.connect(self.__submit_input)
-        self.__layout.addWidget(self.__input_field)
+        self.__input_field.input_field.returnPressed.connect(self.__submit_input)
+        test_widget.addWidget(self.__input_field)
 
-        self.__words_number = InOutField(
-            "Number of words", font, element_height, parent=self
+        self.__words_number = InputField(
+            "Number of words to be randomized", font, element_height, parent=self
         )
         self.__layout.addWidget(self.__words_number)
 
-        self.__submitted_word = InOutField(
-            "Submitted word", font, element_height, readonly=True, parent=self
+        self.__submitted_word = OutputField(
+            "Submitted word", font, element_height, parent=self
         )
-        self.__layout.addWidget(self.__submitted_word)
+        response_widget.addWidget(self.__submitted_word)
 
-        self.__translated_word = InOutField(
-            "Translated word", font, element_height, readonly=True, parent=self
+        self.__translated_word = OutputField(
+            "Translated word", font, element_height, parent=self
         )
-        self.__layout.addWidget(self.__translated_word)
+        response_widget.addWidget(self.__translated_word)
 
-        self.__never_reask_button = PushButton("I learned this word. Don't ask again", font, element_height, parent=self)
+        self.__never_reask_button = PushButton(
+            "I learned this word. Don't ask again", font, element_height, parent=self
+        )
         self.__never_reask_button.clicked.connect(self.__never_reask)
-        self.__layout.addWidget(self.__never_reask_button)
+        response_widget.addWidget(self.__never_reask_button)
 
         self.__submit_button = PushButton("Submit", font, element_height, parent=self)
         self.__submit_button.clicked.connect(self.__submit_input)
-        self.__layout.addWidget(self.__submit_button)
+        test_widget.addWidget(self.__submit_button)
 
-        self.__reset_button = PushButton("Reset to default", font, element_height, parent=self)
+        self.__reset_button = PushButton(
+            "Reset to default", font, element_height, parent=self
+        )
         self.__reset_button.clicked.connect(self.__reset_button_clicked)
         self.__layout.addWidget(self.__reset_button)
 
@@ -74,9 +87,9 @@ class MainWindow(QWidget):
     def set_submitted_word(self, word: str, correct: bool) -> None:
         self.__submitted_word.setText(word)
         if correct:
-            self.__submitted_word.setStyleSheet("background-color: lime")
+            self.__submitted_word.value.setStyleSheet("background-color: lime")
         else:
-            self.__submitted_word.setStyleSheet("background-color: tomato")
+            self.__submitted_word.value.setStyleSheet("background-color: tomato")
 
     def set_words_number(self, number: int) -> None:
         self.__words_number.setText(str(number))
@@ -86,11 +99,11 @@ class MainWindow(QWidget):
         words_number = int(self.__words_number.text())
         self.__submitted_word.setText(input_text)
         self.submitButtonClicked.emit(input_text, words_number)
-        self.__input_field.clear()
+        self.__input_field.input_field.clear()
 
     def __never_reask(self) -> None:
         self.neverReaskButtonClicked.emit()
-        self.__input_field.clear()
+        self.__input_field.input_field.clear()
 
     def __reset_button_clicked(self) -> None:
         self.resetButtonClicked.emit()
--- gui_widgets.py
+++ gui_widgets.py
@@ -4,24 +4,81 @@ from PyQt5.QtWidgets import (
     QWidget,
     QLineEdit,
     QPushButton,
+    QLabel,
+    QVBoxLayout,
 )
 
 
-class InOutField(QLineEdit):
+class BorderedWidget(QWidget):
+    def __init__(self, height: int, parent: QWidget):
+        super().__init__(parent)
+        self.setStyleSheet(
+            """
+            border: 2px solid black;
+            border-radius: 2px;
+            """
+        )
+        self._layout = QVBoxLayout(self)
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self._layout.setStretch(0, 1)
+        self._layout.setStretch(1, 1)
+        self.setLayout(self._layout)
+        self.setFixedHeight(height)
+
+
+class InputField(BorderedWidget):
     def __init__(
         self,
-        placeholder: str,
+        label: str,
         font: QFont,
         height: int,
         parent: QWidget,
-        readonly: bool = False,
     ):
-        super().__init__(parent)
-        self.setPlaceholderText(placeholder)
-        self.setReadOnly(readonly)
-        self.setAlignment(Qt.AlignCenter)
-        self.setFont(font)
-        self.setFixedHeight(height)
+        super().__init__(height, parent)
+
+        self.label = QLabel(label, self)
+        self.label.setFont(font)
+        self.label.setFixedHeight(round(height*0.25))
+        self.label.setAlignment(Qt.AlignCenter)
+        self._layout.addWidget(self.label)
+
+        self.input_field = QLineEdit(self)
+        self.input_field.setFont(font)
+        self.input_field.setFixedHeight(round(height*0.5))
+        self.input_field.setAlignment(Qt.AlignCenter)
+        self._layout.addWidget(self.input_field)
+
+    def text(self) -> str:
+        return self.input_field.text()
+
+    def setText(self, text: str) -> None:
+        self.input_field.setText(text)
+
+
+class OutputField(BorderedWidget):
+    def __init__(
+        self,
+        label: str,
+        font: QFont,
+        height: int,
+        parent: QWidget,
+    ) -> None:
+        super().__init__(height, parent)
+
+        self.label = QLabel(label, self)
+        self.label.setFont(font)
+        self.label.setFixedHeight(round(height*0.25))
+        self.label.setAlignment(Qt.AlignCenter)
+        self._layout.addWidget(self.label)
+
+        self.value = QLabel("", self)
+        self.value.setFont(font)
+        self.value.setFixedHeight(round(height*0.5))
+        self.value.setAlignment(Qt.AlignCenter)
+        self._layout.addWidget(self.value)
+
+    def setText(self, text: str) -> None:
+        self.value.setText(text)
 
 
 class PushButton(QPushButton):
@@ -38,4 +95,15 @@ class PushButton(QPushButton):
         font.setFamily("Arial")
         self.setFont(font)
         self.setStyleSheet(f"background-color: {bg_color}")
-        self.setFixedHeight(height)
+        self.setFixedHeight(round(height * 0.5))
+
+
+class GroupBox(QWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+
+        testVBoxLayout = QVBoxLayout()
+        self.setLayout(testVBoxLayout)
+
+    def addWidget(self, widget: QWidget) -> None:
+        self.layout().addWidget(widget)
